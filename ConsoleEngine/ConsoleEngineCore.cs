b9c3ab3f095f32850bce843d3f77f77853e2b1f5
Grebenuk Denis <32875166+yCatDev@users.noreply.github.com>
2020-04-25 05:38:54 +0300

    Added WinAPI ConsoleEngine fix

--- ConsoleEngine/ConsoleEngineCore.cs
+++ ConsoleEngine/ConsoleEngineCore.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Threading;
 using AbstractEngine.Core;
 using AbstractEngine.Core.Base;
@@ -14,6 +15,18 @@ namespace ConsoleEngine
             Console.Title = title;
             Console.SetWindowSize(w,h);
             Console.SetBufferSize(w+1,h);
+            
+            //DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_MINIMIZE, MF_BYCOMMAND);
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_MAXIMIZE, MF_BYCOMMAND);
+            DeleteMenu(GetSystemMenu(GetConsoleWindow(), false), SC_SIZE, MF_BYCOMMAND);
+            
+            IntPtr stdHandle = GetStdHandle(-11);
+            ConsoleScreenBufferInfoEx bufferInfo = new ConsoleScreenBufferInfoEx();
+            bufferInfo.cbSize = (uint)Marshal.SizeOf(bufferInfo);
+            GetConsoleScreenBufferInfoEx(stdHandle, ref bufferInfo);
+            ++bufferInfo.srWindow.Right;
+            ++bufferInfo.srWindow.Bottom;
+            SetConsoleScreenBufferInfoEx(stdHandle, ref bufferInfo);
         }
         
         protected override void OnRenderStart()
@@ -60,5 +73,82 @@ namespace ConsoleEngine
             };
             DrawPrimitive(d,nextPos);
         }
+
+        #region WinAPI
+        //Fix size
+        const int MF_BYCOMMAND = 0x00000000;
+        const int SC_MINIMIZE = 0xF020;
+        const int SC_MAXIMIZE = 0xF030;
+        const int SC_SIZE = 0xF000;
+
+        [DllImport("user32.dll")]
+        public static extern int DeleteMenu(IntPtr hMenu, int nPosition, int wFlags);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        //Fix console buffer
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr GetStdHandle(int nStdHandle);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool GetConsoleScreenBufferInfoEx(
+            IntPtr hConsoleOutput,
+            ref ConsoleScreenBufferInfoEx ConsoleScreenBufferInfo);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool SetConsoleScreenBufferInfoEx(
+            IntPtr hConsoleOutput,
+            ref ConsoleScreenBufferInfoEx ConsoleScreenBufferInfoEx);
+        
+        
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Coord
+        {
+            public short X, Y;
+
+            public Coord(short x, short y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SmallRect
+        {
+            public short Left, Top, Right, Bottom;
+
+            public SmallRect(short width, short height)
+            {
+                Left = Top = 0;
+                Right = width;
+                Bottom = height;
+            }
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        private struct ConsoleScreenBufferInfoEx
+        {
+            public uint cbSize;
+            public Coord dwSize;
+            public Coord dwCursorPosition;
+            public short wAttributes;
+            public SmallRect srWindow;
+            public Coord dwMaximumWindowSize;
+            public ushort wPopupAttributes;
+            public bool bFullscreenSupported;
+
+            public Colorref black, darkBlue, darkGreen, darkCyan, darkRed, darkMagenta, darkYellow, gray, darkGray, blue, green, cyan, red, magenta, yellow, white;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Colorref
+        {
+            public uint ColorDWORD;
+        }
+        
+        #endregion
     }
 }
